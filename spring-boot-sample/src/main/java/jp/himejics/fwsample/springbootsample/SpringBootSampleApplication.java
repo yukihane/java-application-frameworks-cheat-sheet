a5b4c8cc949278825292a968195f294c2ebfcf85
yukihane <yukihane.feather@gmail.com>
2020-04-02 20:25:16 +0900

    spring-boot-sample プロジェクト追加

---
+++ spring-boot-sample/src/main/java/jp/himejics/fwsample/springbootsample/SpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package jp.himejics.fwsample.springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleApplication.class, args);
+	}
+
+}
